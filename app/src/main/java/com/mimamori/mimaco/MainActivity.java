e5d6ec986fe7b1f30f89b7563f938585500ebd08
ke_ix1 <xkei4dev@gmail.com>
2018-06-11 21:42:30 +0900

    位置情報を扱えるよう変更

--- app/src/main/java/com/mimamori/mimaco/MainActivity.java
+++ app/src/main/java/com/mimamori/mimaco/MainActivity.java
@@ -1,10 +1,16 @@
 package com.mimamori.mimaco;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import otoshimono.com.lost.mamorio.sdk.Mamorio;
 import otoshimono.com.lost.mamorio.sdk.MamorioSDK;
@@ -12,19 +18,37 @@ import otoshimono.com.lost.mamorio.sdk.User;
 import otoshimono.com.lost.mamorio.sdk.Error;
 
 
-public class MainActivity extends AppCompatActivity implements MimamorioFragment.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity implements MimamorioFragment.OnFragmentInteractionListener, MimamoRareFragment.OnFragmentInteractionListener{
 
     private static String TAG = MainActivity.class.getCanonicalName();
+    private final int REQUEST_PERMISSION = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        MimamoRareFragment mimamoRareFragment = new MimamoRareFragment();
+//        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//        transaction.add(R.id.fragmentMimarioContainer, mimamoRareFragment);
+//        transaction.commit();
+
+        if(Build.VERSION.SDK_INT >= 23){
+            checkPermission();
+        }
+        else{
+            locationActivity();
+        }
+
+    }
+
+    private void locationActivity() {
+
         MimamorioFragment mimamorioFragment = new MimamorioFragment();
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.add(R.id.fragmentMimarioContainer, mimamorioFragment);
         transaction.commit();
+
     }
 
     @Override
@@ -37,5 +61,62 @@ public class MainActivity extends AppCompatActivity implements MimamorioFragment
     public void onFragmentInteraction(Uri uri) {
 
     }
+
+    // 位置情報許可の確認
+    public void checkPermission() {
+        // 既に許可している
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)==
+                PackageManager.PERMISSION_GRANTED){
+
+            locationActivity();
+        }
+        // 拒否していた場合
+        else{
+            requestLocationPermission();
+        }
+    }
+
+    // 許可を求める
+    private void requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_PERMISSION);
+
+        } else {
+            Toast toast = Toast.makeText(this,
+                    "許可されないとアプリが実行できません", Toast.LENGTH_SHORT);
+            toast.show();
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,},
+                    REQUEST_PERMISSION);
+
+        }
+    }
+
+    // 結果の受け取り
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode,
+            @NonNull String[] permissions,
+            @NonNull int[] grantResults) {
+
+        if (requestCode == REQUEST_PERMISSION) {
+            // 使用が許可された
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                locationActivity();
+
+            } else {
+                // それでも拒否された時の対応
+                Toast toast = Toast.makeText(this,
+                        "これ以上なにもできません", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+    }
+
 
 }
